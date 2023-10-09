8a655994dc59298e0f187209cd793e41745f7ac2
EGOIST <tianyuma4869@gmail.com>
2023-10-09 15:31:10 +1100

    add ics to json

---
+++ src/main/java/com/example/campuslink/services/PullICS.java
@@ -0,0 +1,14 @@
+package com.example.campuslink.services;
+
+import springfox.documentation.spring.web.json.Json;
+
+public class PullICS {
+    public PullICS() {
+    }
+
+    public Json pullICStoJson(String url) {
+        //sending a get request
+
+        return null;
+    }
+}
